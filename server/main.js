7c2bdc9f78c47cabca02a3f08ffc4ac659eb8f6e
Sofi <sofi+git@mailbox.org>
2024-04-02 12:49:08 +0200

    feat: add morgan middleware logger to example

--- server/main.js
+++ server/main.js
@@ -1,7 +1,12 @@
 // This is the example "Hello World" from:
 // https://expressjs.com/en/starter/hello-world.html
 const express = require('express');
+const morgan = require('morgan');
+
+const logger = morgan('common');
 const app = express();
+app.use(logger);
+
 // To see the relevant ports we can use, you can run the following command in an `ssh` session on the server:
 // `grep "# HTTP reverse proxy configurations" -A 10 /srv/www/cs-24-sw-2-05.p2datsw.cs.aau.dk/conf-https/mod_proxy.conf`
 // This has currently been set to point to /node0/ in that configuration.
