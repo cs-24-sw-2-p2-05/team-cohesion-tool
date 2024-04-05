09af271356ab02c1c3aad0ded8af3e0142dc94e9
naitsa <naitsabot@gmail.com>
2024-04-05 13:44:50 +0200

    filestructure, serve static files in public folder, added index, added css file

--- src/main.js
+++ src/main.js
@@ -9,13 +9,16 @@ const app = express();
 app.use(logger); // Enable logging of requests.
 app.set('trust proxy', 'loopback'); // Trust locally hosted proxies for IP data.
 
+app.use(express.static('src/public')); // Serve static files from the public directory.
+
 // To see the relevant ports we can use, you can run the following command in an `ssh` session on the server:
 // `grep "# HTTP reverse proxy configurations" -A 10 /srv/www/cs-24-sw-2-05.p2datsw.cs.aau.dk/conf-https/mod_proxy.conf`
 // This has currently been set to point to /node0/ in that configuration.
 const port = 3350;
 
 app.get('/', (req, res) => {
-  res.send('<t>Hello there</t>\n<h1>General Kenobi</h1>');
+  console.log('./public/html/index.html', { root: __dirname });
+  res.sendFile('./public/html/index.html', { root: __dirname });
 });
 
 app.listen(port, () => {
