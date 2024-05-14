5797de824ecd15f61bf0daee02601efd8796d3a3
SilleKorsbakke <151042508+SilleKorsbakke@users.noreply.github.com>
2024-05-14 21:21:32 +0200

    The Activity Ranker and sort
    changes in app.mjs must be moved to algorithm.mjs in a fitting place. There must be added a import for activity suggester, as well as a call in routing: call must be: ActivityRanker(arr3, database["profiles"], database["activities"]);

--- src/app.mjs
+++ src/app.mjs
@@ -34,3 +34,116 @@ function validateUsername(userName) {
     }
     throw new Error(ValidationError);
 }
+
+export { ActivityRanker }
+
+//Activity Ranker
+function ActivityRanker(list_consecutive_times, profiles, activities){  
+    let lengthOfList = list_consecutive_times.length; 
+    //console.log(list_consecutive_times);
+    //console.log("length:"+lengthOfList); 
+   
+    for (let i = 0; i < lengthOfList; i++){
+        let scored_activities = {}
+
+//finding 1
+
+        list_consecutive_times[i].users.forEach(profile => {
+           // console.log(profile);
+            let user = profile;
+
+            for(const key in activities){
+                let activity_key = key;
+
+                if (activities[activity_key].time_interval <= list_consecutive_times[i].interval_list.length){
+  //                  console.log(list_consecutive_times[i] + activity_key);
+
+                    if(profiles[user].activity_ids.includes(activity_key)){
+                       // console.log(activity_key);
+
+                        if(Object.keys(scored_activities).includes(activity_key) == false){
+                            scored_activities[activity_key] = 1;
+                        }
+                        else {
+                        scored_activities[activity_key] += 1;
+                        }
+                       
+                    }
+
+            }
+            
+    }})
+
+
+    list_consecutive_times[i].scored_activities = scored_activities;
+   // console.log(list_consecutive_times[i]);
+
+   // console.log("including 0.5");
+
+// finding 0.5
+
+    list_consecutive_times[i].users.forEach(profile => {
+        // console.log(profile);
+         let user = profile;
+
+        //console.log("hej")
+
+         for(const key in scored_activities){
+            //console.log("hej")
+            let activity_key = key;
+//for of
+            let limit = 0;
+            while (limit == 0) {
+               // console.log("hej")
+            if(profiles[user].activity_ids.includes(activity_key) == false){   
+                activities[activity_key].all_interest_ids.forEach (interest => {
+                   // console.log(user);
+                   // console.log(activity_key);
+
+
+                    for(let i = 0; i < profiles[user].activity_ids.length; i++){
+                    //console.log(profiles[user].activity_ids[i]);
+                    let personal_activity = profiles[user].activity_ids[i];
+                    if(activities[personal_activity].all_interest_ids.includes(interest) == true){
+                       // console.log("personal");
+                       // console.log(user);
+                      //  console.log(personal_activity);
+                        scored_activities[activity_key] += 0.5;
+                        break;   
+                    }
+                    }
+                } 
+            )
+            limit += 1;
+            }
+            limit += 1;
+        }}})
+
+        list_consecutive_times[i].scored_activities = scored_activities;
+        console.log(list_consecutive_times[i]);
+
+        }
+
+        //Sorting each "scored activities"
+
+         for (let i = 0; i < lengthOfList; i++){
+           let convertDirtoArr = Object.entries(list_consecutive_times[i].scored_activities).map(([key, value]) => ({ [key]: value }));
+
+           console.log("unsorted", convertDirtoArr);
+
+           let sortedArray = convertDirtoArr.sort((a, b) => {
+            const valueA = Object.values(a)[0];
+            const valueB = Object.values(b)[0];
+
+            return valueB - valueA;
+           });
+
+
+          list_consecutive_times[i].scored_activities = sortedArray;
+           
+       console.log("sorted", sortedArray);
+
+        }    
+
+//        console.log(JSON.stringify(list_consecutive_times, null, 2));
+}
