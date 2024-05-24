343391074102f585c0b3b1954e6443e5b407109c
Sofi <sofi+git@mailbox.org>
2024-05-24 09:36:19 +0200

    hack: use relative paths for api queries
    This is done because our server gets exposed under the subpath
    `/node0/` through a reverse proxy which forwards the requests to `/`.
    
    This makes it quite difficult to know what the true root of our API
    endpoint is at runtime. This is annoying, but since our single-page
    web client is exposed at the top level, we can misuse this fact to make
    the client's api requests be relative to wherever that is.
    
    Better solution would likely be to configure it with a `config.js` like
    file, and template our client side javascript to the correct baseUrl.
    But using relative paths also work well here, so its a nice hack.

--- src/public/js/comms.mjs
+++ src/public/js/comms.mjs
@@ -9,6 +9,8 @@ export { fetchCalculatedData }
 export { fetchProfile, fetchTeam, fetchActivity, fetchInterest, fetchAllInterests, fetchAllTeams, fetchAllActivitiesFromInterest, fetchAllProfilesFromTeam };
 export { postProfile, postTeam, postActivity, postInterest };
 
+const baseDir = './';
+
 // Log JSON data to console
 function consoleLogJSONData(data) {
     Object.keys(data).forEach(key => { console.log(key + ": " + data[key]); });
@@ -30,14 +32,14 @@ function verifyJson(response) {
 
 // GET and parse JSON data from server
 function getJson(url) {
-    return fetch(url)
+    return fetch(baseDir + url)
     .then(verifyJson)
     .then(data => {
         //console.log("FetchGet: " + url + ": ", data);
-        console.log("FetchGet: " + url);
+        console.log("FetchGet: " + baseDir + url);
         return data;
     })
-    .catch(error => { console.error("FetchGet: " + url + ": " + error); });
+    .catch(error => { console.error("FetchGet: " + baseDir + url + ": " + error); });
 }
 
 // POST a JSON object to the server, thus adding or updating data in the database
@@ -50,26 +52,26 @@ function postJson(url, data) {
         },
         body: JSON.stringify(data)
     };
-    return fetch(url, fetchoptions)
-    .then(response => { 
-        if (response.ok) { 
-            return response.ok; 
-        } else { 
-            throw new Error(response.statusText); 
-        } 
+    return fetch(baseDir + url, fetchoptions)
+    .then(response => {
+        if (response.ok) {
+            return response.ok;
+        } else {
+            throw new Error(response.statusText);
+        }
     })
     .then(data => {
         //console.log("FetchPost: " + url + ": ", data);
-        console.log("FetchPost: " + url);
+        console.log("FetchPost: " + baseDir + url);
         return data;
     })
-    .catch(error => { console.error("fetch: " + url + ": " + error); });
+    .catch(error => { console.error("fetch: " + baseDir + url + ": " + error); });
 }
 
 
 // fetch helper function to get calculated data from server
 function fetchCalculatedData(teamIDName) {
-    return getJson("/teams/" + teamIDName + "/calculate");
+    return getJson("teams/" + teamIDName + "/calculate");
 }
 
 
@@ -78,43 +80,43 @@ function fetchCalculatedData(teamIDName) {
 // Fetch profile data from server
 function fetchProfile(profileUsername) {
     //console.log("fetchProfile: " + profileUsername);
-    return getJson("/profiles/" + profileUsername);
+    return getJson("profiles/" + profileUsername);
 }
 
 // Fetch team data from server
 function fetchTeam(teamIDName) {
     //console.log("fetchTeam: " + teamNumber);
-    return getJson("/teams/" + teamIDName);
+    return getJson("teams/" + teamIDName);
 }
 
 // Fetch activity data from server
 function fetchActivity(activityNumber) {
-    return getJson("/activities/" + activityNumber);
+    return getJson("activities/" + activityNumber);
 }
 
 // Fetch interest data from server
 function fetchInterest(interestNumber) {
-    return getJson("/interests/" + interestNumber);
+    return getJson("interests/" + interestNumber);
 }
 
 // Fetch all interests data from server
 function fetchAllInterests() {
-    return getJson("/interests");
+    return getJson("interests");
 }
 
 // Fetch all teams data from server
 function fetchAllTeams() {
-    return getJson("/teams");
+    return getJson("teams");
 }
 
 // Fetch all activities data in relation to an main interest from server
 function fetchAllActivitiesFromInterest(interestNumber) {
-    return getJson("/activities" + "/interests/" + interestNumber);
+    return getJson("activities/" + "interests/" + interestNumber);
 }
 
 // Fetch all profiles data in relation to a team from server
 function fetchAllProfilesFromTeam(teamIdName) {
-    return getJson("/profiles" + "/teams/" + teamIdName);
+    return getJson("profiles/" + "teams/" + teamIdName);
 }
 
 
@@ -123,23 +125,23 @@ function fetchAllProfilesFromTeam(teamIdName) {
 // Fetch POST profile data to server
 function postProfile(profileUsername, data) {
     console.log("fetchProfile: " + profileUsername, data);
-    return postJson("/profiles/" + profileUsername, data);
+    return postJson("profiles/" + profileUsername, data);
 }
 
 // Fetch POST team data to server
 function postTeam(teamIdName, data) {
     console.log("fetchTeam: " + teamIdName, data);
-    return postJson("/teams/" + teamIdName, data);
+    return postJson("teams/" + teamIdName, data);
 }
 
 // Fetch POST activity data to server
 function postActivity(activityNumber, data) {
     console.log("fetchActivity: " + activityNumber, data);
-    return postJson("/activities/" + activityNumber, data);
+    return postJson("activities/" + activityNumber, data);
 }
 
 // Fetch POST interest data to server
 function postInterest(interestNumber, data) {
     console.log("fetchInterest: " + interestNumber, data);
-    return postJson("/interests/" + interestNumber, data);
+    return postJson("interests/" + interestNumber, data);
 }
